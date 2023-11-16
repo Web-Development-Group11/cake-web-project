a9b25faca9bed785504b8d0c9133c52e359b056c
NQD190523 <duc19052003@gmail.com>
2023-11-16 17:18:09 +0700

    update outline

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.render('hello');
 });
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import { BrowserRouter,  Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Product from './pages/Product';
+import Product_detail from './pages/Product_detail';
+import Support from './pages/Support';
+import Payment from './pages/Payment';
+import Account from './pages/Account';
+import Cart from './pages/Cart';
+import Introduction from './pages/Introduction';
+import Blog from './pages/Blog';
 
 
 function App() {
@@ -9,6 +18,16 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={< Home />} />
+        <Route path="/login" element={< Login />} />
+        <Route path="/register" element={< Register />} />
+        <Route path="/product" element={< Product />} />
+        <Route path="product_detail" element={< Product_detail />} />
+        <Route path="/support" element={< Support/>} />
+        <Route path="/payment" element={< Payment />} />
+        <Route path="/account" element={< Account />} />
+        <Route path="/cart" element={< Cart />} />
+        <Route path="/introduction" element={< Introduction />} />
+        <Route path="/blog" element={< Blog />} />
       </Routes>
       </BrowserRouter>
     </div>
