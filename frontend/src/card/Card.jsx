ea5e9dfdb3b4993304b1546a2ef882225ca77aee
NgocDungK214060390 <huynhdung4032@gmail.com>
2023-11-27 21:51:36 +0700

    Update Card.jsx

--- frontend/src/card/Card.jsx
+++ frontend/src/card/Card.jsx
@@ -43,56 +43,43 @@ export const Card = ({ status, className }) => {
         <div className="line"></div>
         <div className="rate">
           <div className="div">{pRate}</div>
+          <div className="flex">
+            {/* Yellow Star */}
+            {[...Array(Number(item.rating))].map((index) => {
+              return (
+                <span key={index}>
+                <svg
+                  className=""
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                </span>
+                          );
+                        })}
+                        {/* White Star */}
+                        {[...Array(5 - Number(item.rating))].map((index) => {
+                          return (
+                            <span key={index}>
+                              <svg
+                                className="w-4 h-4 fill-current text-gray-300"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                              </svg>
+                            </span>
+                          );
+                        })}
+                      </div>"
           <div className="star">
-            <img
-              alt="Star"
-              src={
-                state.status === "hover"
-                  ? <FaRegStar />
-                  : <FaRegStar />
-              }
-            />
-            <img
-          
-              alt="Star"
-              src={
-                state.status === "hover"
-                  ? <FaRegStar />
-                  : <FaRegStar />
-              }
-            />
-            <img
-        
-              alt="Star"
-              src={
-                state.status === "hover"
-                  ? <FaRegStar />
-                  : <FaRegStar />
-                }
-              />
-            <img
-              
-              alt="Star"
-              src={
-                state.status === "hover"
-                  ? <FaRegStar />
-                  : <FaRegStar />
-                }
-              />
-            <img
-              
-              alt="Star"
-              src={
-                state.status === "hover"
-                  ? <FaRegStar />
-                  : <FaRegStar />
-                }
-              />
+            <FaRegStar color="red"/>
 
-            </div>
-          
+
           </div>
-          <div className="text-wrapper-2">150,000đ</div>
+          <div className="text-wrapper-2">{pPrice}</div>
           <div className="add-to-cart">
             <FaShoppingCart  className="icons" color={state.status === "hover" ? "#47271C" : "white"} />
         </div>
@@ -100,6 +87,7 @@ export const Card = ({ status, className }) => {
     </div>
   );
 };
+)
 
 function reducer(state, action) {
   switch (action) {
