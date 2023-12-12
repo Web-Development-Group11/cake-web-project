21737bf796e455410dd99e5600db05891e3a506b
NQD190523 <duc19052003@gmail.com>
2023-12-13 00:37:27 +0700

    merge card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,  Routes, Route } from 'react-router-dom';
-import Home from './pages/home1/Home';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Product from './pages/product/Product';
