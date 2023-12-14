d5098f03ec4bfdd5d459ac635bdb39065bc36cc7
NgocDungK214060390 <huynhdung4032@gmail.com>
2023-12-14 14:55:15 +0700

    up

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Button from '../../components/button/Button'
 import Navbar from '../../components/header/NavBar'
 import Footer from '../../components/footer/Footer'
+import Card from '../../components/card/Card'
 
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-        <p>hello world</p>
+        <h1>
+        Welcome to React
+        </h1>
         <Button type ="btn1 primary " >Liên hệ </Button>
-      <Footer/>
+        <Card></Card>
+      {/* <Footer/> */}
     </div>
   )
 }
