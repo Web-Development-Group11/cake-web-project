4af7ae87374d84d9f3549c455f388afbeabf026d
giaphuong <giaphuong2409@gmail.com>
2023-12-15 10:57:47 +0700

    Password component

--- frontend/src/components/textField/TextField.jsx
+++ frontend/src/components/textField/TextField.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './TextField.css';
 
-const TextField = ({value, onChange, placeholder}) => {
+const TextField = ({value, placeholder}) => {
   return (
     <div>
       <input className='text-field__input'
         type="text"
         value={value}
-        onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
       />
     </div>
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import Button from '../../components/button/Button';
 import Footer from '../../components/footer/Footer';
+import Password from '../../components/password/Password';
+import TextField from '../../components/textField/TextField';
+import Register from '../register/Register';
 import './Home.css';
 
 const Home = () =>{
     return (
         <div>
-            <Footer></Footer>
+            <Password></Password>
         </div>
     )
 }
