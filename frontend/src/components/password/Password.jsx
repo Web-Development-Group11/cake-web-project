8e49b71a3b38605e7e0f8323d53c63ff4f41a44e
giaphuong <giaphuong2409@gmail.com>
2023-12-15 17:16:44 +0700

    nè

--- frontend/src/components/password/Password.jsx
+++ frontend/src/components/password/Password.jsx
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import './Password.css';
-// import "../../Variable.css";
-// import { FaEye } from "react-icons/fa";
-// import { FaEyeSlash } from "react-icons/fa";
+import React from 'react';
+import './Password.css';
+import "../../Variable.css";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
-// const Password = () => {
-//     const [showPassword, setShowPassword] = React.useState(false);
+const Password = () => {
+    const [showPassword, setShowPassword] = React.useState(false);
 
-//     const icon = showPassword ? <FaEye /> : <FaEyeSlash />;
+    const icon = showPassword ? <FaEye /> : <FaEyeSlash />;
 
-//     const handleClick = () => {
-//         setShowPassword(!showPassword);
-//     };
+    const handleClick = () => {
+        setShowPassword(!showPassword);
+    };
 
-//     return (
-//         <div className='password-container'>
-//             <input className='password__input body--2'
-//             type={showPassword ? "text" : "password"}
-//             placeholder="Enter Password"
-//             />
-//             <span className='password__click' onClick={handleClick}>{icon}</span>
-//         </div>
-//     );
-//   };
+    return (
+        <div className='password-container'>
+            <input className='password__input body--2'
+            type={showPassword ? "text" : "password"}
+            placeholder="Enter Password"
+            />
+            <span className='password__click' onClick={handleClick}>{icon}</span>
+        </div>
+    );
+  };
 
-// export default Password;
+export default Password;
 
