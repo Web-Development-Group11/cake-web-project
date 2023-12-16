9c9431bd1171774ca6b0d67d86b328410a524198
NgocDungK214060390 <huynhdung4032@gmail.com>
2023-12-16 13:01:45 +0700

    up

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./Cart.css";
 import Button from "../../components/button/Button";
 import Navbar from '../../components/header/NavBar'
 import Footer from '../../components/footer/Footer'
+import cartStyles from "./Cart.module.css"
 const Cart = () => {
   return (
     <div >
