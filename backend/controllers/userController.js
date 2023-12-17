13cb6bb6eb8db4c75011e4147870d3a5a981a536
NQD190523 <duc19052003@gmail.com>
2023-12-17 20:01:03 +0700

    update login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,23 +3,27 @@ import  bcrypt  from "bcrypt";
 
 
 const user = new PrismaClient().user;
-
+// thêm người dùng mới
 const createNewUser = async (req,res) => {
   try {
   const { name, email, password, username, phoneNumber } = req.body;
-  console.log(typeof email);
-  // if (typeof email !== "string") return res.status(404).json( {message: "invalid email"});
-  // const exitingUser = await user.findUnique({ where : {email : email} });
-  // if (exitingUser) { return res.status(400).json({ message : "User already exist" }); }
+//kiểm tra validate
+  if (typeof email !== "string") return res.status(404).json( {message: "invalid email"});
+//kiểm tra người dùng tồn tại bằng email
+  const exitingEmail = await user.findUnique({  where : {email : email }  });
+  //kiểm tra người dùng tồn tại bằng phoneNumber
+    const exitingPhoneNumber = await user.findUnique({  where : {phoneNumber : phoneNumber }  });
+  if (exitingEmail || exitingPhoneNumber) { return res.status(400).json({ message : "User already exist" }); }
+  //mã hóa mật khẩu
   const hashPassword = await  bcrypt.hash(password, 10);
-  const phone = parseInt(phoneNumber);
+  //tạo người dùng mới 
   const newUser = await user.create({
       data: {
       name ,
       email ,
       password : hashPassword,
       username ,
-      phoneNumber,
+      phoneNumber ,
       },
   });
   res.status(200).json({ data : newUser});
@@ -27,4 +31,14 @@ const createNewUser = async (req,res) => {
   res.status(500).json({ message : error.message });
 }
 };
+
+const loginUser = async (req,res) => {
+  try {
+    const { username, password } = req.body;
+    const exitingUser = await user.findUnique({ where : {email : username} });
+  } catch (err) {
+    res.status(500).json({ message : err.message });
+  }
+};
+
 export default createNewUser;
