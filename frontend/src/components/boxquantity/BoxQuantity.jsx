05dbcfa217ef485c246c76e2215bdcaa3b5417fa
NgocDungK214060390 <huynhdung4032@gmail.com>
2023-12-18 09:21:06 +0700

    up

--- frontend/src/components/boxquantity/BoxQuantity.jsx
+++ frontend/src/components/boxquantity/BoxQuantity.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import boxQtyStyles from "./Boxquantity.module.css";
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
 
-const BoxQuantityComponent = () => {
+const BoxQuantityComponent = ({ height = "2.5rem" }) => {
   const [quantity, setQuantity] = useState("1");
+  const minusIconRef = useRef(null);
+  const plusIconRef = useRef(null);
+
+  useEffect(() => {
+    if (minusIconRef.current) {
+      const parentWidth = minusIconRef.current.parentNode.offsetWidth;
+      minusIconRef.current.style.fontSize = `${parentWidth}rem`;
+    }
+
+    if (plusIconRef.current) {
+      const parentWidth = plusIconRef.current.parentNode.offsetWidth;
+      plusIconRef.current.style.fontSize = `${parentWidth}rem`;
+    }
+  }, [height]);
 
   const incrementQuantity = () => {
     setQuantity((prevQuantity) => {
@@ -25,40 +41,52 @@ const BoxQuantityComponent = () => {
     }
   };
 
+  const buttonHeight = parseFloat(height);
+  const calculateFontSize = () => {
+    return `calc(${buttonHeight / 2}rem)`;
+  };
+
+  const minWidth = `${buttonHeight * 3}rem`; 
+  const maxWidth = `${buttonHeight *4}rem`; 
+  
+  console.log("minWidth:", minWidth);
+  console.log("maxWidth:", maxWidth);
+  
+
   return (
     <>
-    <div className={boxQtyStyles.button__overlap}>
-      <div className={boxQtyStyles.button__quantity}>
+      <div
+        className={boxQtyStyles.button__overlap}
+        style={{ height: height,  minWidth: minWidth, maxWidth: maxWidth }}
+        
+      >
         <div
-          className={`${boxQtyStyles.buttonWrapper} ${
-            parseInt(quantity) === 1 ? boxQtyStyles.disabled : ""}`}
+          className={`${boxQtyStyles.button__quantity} ${parseInt(quantity) === 1 ? boxQtyStyles.disabled : ""
+            }`}
           onClick={decrementQuantity}
         >
-          <span className={boxQtyStyles.quantityText}>-</span>
+          <FaMinus className={boxQtyStyles.customIcon} ref={minusIconRef} style={{ fontSize: calculateFontSize() }} />
+        </div>
+
+        <div className={boxQtyStyles.input__quantity}>
+          <input
+            className={`body--1 ${boxQtyStyles.inputQty}`}
+            type="text"
+            step="1"
+            min="1"
+            value={quantity}
+            onChange={handleQuantityChange}
+            style={{ fontSize: calculateFontSize() }}
+          />
+        </div>
+
+        <div className={boxQtyStyles.button__quantity} onClick={incrementQuantity}>
+
+          <FaPlus className={boxQtyStyles.customIcon} ref={plusIconRef} style={{ fontSize: calculateFontSize() }} />
+
         </div>
       </div>
-
-      <div className={boxQtyStyles.input__quantity}>
-        <input
-          className={`body--1 ${boxQtyStyles.inputQty}`}
-          type="text"
-          step="1"
-          min="1"
-          value={quantity}
-          onChange={handleQuantityChange}
-        />
-      </div>
-
-      <div className={boxQtyStyles.button__quantity}>
-        <div
-          className={boxQtyStyles.buttonWrapper}
-          onClick={incrementQuantity}
-        >
-          <span className={boxQtyStyles.quantityText}>+</span>
-        </div>
-      </div>
-    </div>
-   </>
+    </>
   );
 };
 
