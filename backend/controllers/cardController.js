30d70901a40562653e194407c556c79f4d2de687
NQD190523 <duc19052003@gmail.com>
2023-12-18 23:50:16 +0700

    update randombox API

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -13,3 +13,20 @@ export const getProduct =  async (req, res) => {
         })
     };
 }
+
+export const getRandomProduct = async (req, res) => {
+    try {
+        const count = await product.count(); // Đếm số lượng bản ghi
+        const randomOffset = Math.floor(Math.random() * count); // Tạo offset ngẫu nhiên
+        const info = await product.findFirst({
+           skip : randomOffset,
+        });
+        res.status(200).json({data: info})
+    } catch(error) {
+        res.status(500).json({message: error.message})
+
+    } finally { async () => {
+        await product.$disconnect();
+        }   
+    }
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,10 @@
-import { getProduct } from "../controllers/cardController.js";
+import { getProduct, getRandomProduct } from "../controllers/cardController.js";
 import { Router } from "express";
 
 const productRouter = new Router();
 
 productRouter.get('/products', getProduct);
+
+productRouter.get('/products/random', getRandomProduct);
 
 export default productRouter;
